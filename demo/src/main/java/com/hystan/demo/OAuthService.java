a3c48e9e1333e174259628c84c982a00666f83fc
Davi <hystancorp@gmail.com>
2026-05-13 16:38:21 -0300

    oaurg

--- demo/src/main/java/com/hystan/demo/OAuthService.java
+++ demo/src/main/java/com/hystan/demo/OAuthService.java
@@ -16,27 +16,9 @@ public class OAuthService extends DefaultOAuth2UserService {
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest request) {
-        try {
-            System.out.println("=== OAUTH INICIADO ===");
-            OAuth2User user = super.loadUser(request);
-            System.out.println("=== OAUTH OK: " + user.getAttribute("email"));
-            
-            String email = user.getAttribute("email");
-            String nome  = user.getAttribute("name");
-            String foto  = user.getAttribute("picture");
-
-            repo.findByEmail(email).orElseGet(() -> {
-                Usuario novo = new Usuario(email, nome, foto);
-                return repo.save(novo);
-            });
-
-            System.out.println("=== USUARIO SALVO ===");
-            return user;
-
-        } catch (Exception e) {
-            System.out.println("=== ERRO OAUTH: " + e.getMessage());
-            e.printStackTrace();
-            throw e;
-        }
+        System.out.println("=== OAUTH INICIADO ===");
+        OAuth2User user = super.loadUser(request);
+        System.out.println("=== OAUTH OK: " + user.getAttribute("email"));
+        return user;
     }
 }
